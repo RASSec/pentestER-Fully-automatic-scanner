8fab812ff6179b272674f2d7ac2acca9d5447b06
mingze <exp_data@126.com>
2016-11-27 10:42:45 +0800

    update
    update

---
+++ thirdlib/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+"""
+Copyright (c) 2014-2015 ourren
+author: ourren <i@ourren.com>
+"""
+
+
+def main():
+    print 'enter main...'
+
+
+if __name__ == "__main__":
+    main()
